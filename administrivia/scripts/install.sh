d56605111ce15b8d3fe31860e9d1138613c419e5
Emily <metaranha@gmail.com>
2018-10-02 14:17:49 -0400

    added file moving

--- administrivia/scripts/install.sh
+++ administrivia/scripts/install.sh
@@ -15,3 +15,8 @@ git clone https://github.com/emilymaxima/Hack-Lab.git
 #adding backdoor account
 adduser kali_user
 
+#move connection scripts to where they need to be
+
+echo "moving files around"
+mv OS_Files/wpa_supplicant1.conf /etc/wpa_supplicant.conf
+mv OS_Files/interfaces /etc/network/interfaces
